3d63fa7df58f2798148e503d27bc281e880db1f2
fjita-ryota <167516868+fjita-ryota@users.noreply.github.com>
2025-04-18 18:26:09 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//JavaScript
+// document.getElementById("myButton").addEventListener("click" , function () {
+//     alert("ボタンを押したよ");
+// });
+
+//jQuery
+$(function() {
+    $("#myButon").click(function(){
+        alert("ボタンを押したぞ");
+    });
+});
